f891d98451b3751730130f8083b02ed47d1ca717
Origins_Eternity <origins.eternity@outlook.com>
2026-09-21 10:52:38 +0800

    Use real eye position to calculate brightness. Fix #74

--- src/main/java/com/origins_eternity/sanity/utils/Utils.java
+++ src/main/java/com/origins_eternity/sanity/utils/Utils.java
@@ -66,7 +66,7 @@ public class Utils {
         if (player.getAir() < 90) {
             value -= Mechanics.choking;
         }
-        if (player.world.getLight(new BlockPos(player).up((int) player.eyeHeight), true) < 4) {
+        if (player.world.getLight(new BlockPos(player.getPositionEyes(1.0f)), true) < 4) {
             if (!player.isPotionActive(MobEffects.NIGHT_VISION)) {
                 value -= Mechanics.dark;
             }
